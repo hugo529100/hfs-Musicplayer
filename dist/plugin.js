f8330b5d92014fa2205533279665cbca7f8b07b8
Hug3O <131569471+hugo529100@users.noreply.github.com>
2026-09-09 23:35:07 +0800

    Add files via upload

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.description = "A clean and pure music player with high-resolution audio support"
-exports.version = 7.1
+exports.version = 7.2
 exports.apiRequired = 9.5
 exports.repo = "Hug3O/Musicplayer+"
 exports.frontend_css = "style.css"
